184b8c4abb01ee5d6fabdceaa064952f38876b79
Artem <smtyper.contact@gmail.com>
2022-09-16 18:21:09 +0300

    Implement caesar cipher

---
+++ Ciphers/Cipher.cs
@@ -0,0 +1,32 @@
+﻿namespace Ciphers;
+
+public abstract class Cipher
+{
+    protected readonly char[] Alphabet;
+
+    protected Cipher(IEnumerable<char> alphabet) => Alphabet = alphabet.Distinct().ToArray();
+
+    public string Encrypt(string text)
+    {
+        EnsureIsValidText(text);
+
+        return EncryptText(text);
+    }
+
+    public string Decrypt(string text)
+    {
+        EnsureIsValidText(text);
+
+        return DecryptText(text);
+    }
+
+    protected abstract string EncryptText(string text);
+
+    protected abstract string DecryptText(string text);
+
+    private void EnsureIsValidText(string text)
+    {
+        if (text.Any(chr => !Alphabet.Contains(chr)))
+            throw new ArgumentException("The text contains characters outside the current alphabet.");
+    }
+}
---
+++ Ciphers/Transposition/CaesarCipher.cs
@@ -0,0 +1,27 @@
+﻿namespace Ciphers.Transposition;
+
+public class CaesarCipher : Cipher
+{
+    protected readonly int Key;
+
+    protected readonly IReadOnlyDictionary<char, char> EncryptionCharMapping;
+    protected readonly IReadOnlyDictionary<char, char> DecryptionCharMapping;
+
+    public CaesarCipher(IReadOnlyCollection<char> alphabet, int key) : base(alphabet)
+    {
+        Key = key;
+
+        var charMapping = alphabet
+            .Select((chr, index) => (chr, index))
+            .ToDictionary(pair => pair.chr, pair => Alphabet[pair.index + Key]);
+
+        EncryptionCharMapping = charMapping;
+        DecryptionCharMapping = charMapping.ToDictionary(pair => pair.Value, pair => pair.Key);
+    }
+
+    protected override string EncryptText(string text) => string.Concat(text
+        .Select(chr => EncryptionCharMapping[chr]));
+
+    protected override string DecryptText(string text) => string.Concat(text
+        .Select(chr => DecryptionCharMapping[chr]));
+}
