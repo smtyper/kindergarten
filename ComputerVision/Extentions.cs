1a05003e4f5618ede6fd001cd7138514398e6f49
Artem <smtyper.contact@gmail.com>
2022-09-25 22:29:30 +0300

    Implement grayscale and noise

---
+++ ComputerVision/Extentions.cs
@@ -0,0 +1,31 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace ComputerVision;
+
+internal static class Extentions
+{
+    public static double NextGaussian(this Random random, double mean, double sigma)
+    {
+        // Box–Muller transform
+        var x = random.NextDouble();
+        var y = random.NextDouble();
+
+        var z = Math.Sin(2 * Math.PI * y) * Math.Sqrt(-2 * Math.Log(x));
+        var result = mean + (sigma * z);
+
+        return result;
+    }
+
+    public static Image ForEachPixel<T>(this Image<T> image, Func<T, T> function) where T : unmanaged, IPixel<T>
+    {
+        foreach (var (x, y) in Enumerable
+                     .Range(0, image.Width)
+                     .SelectMany(x => Enumerable
+                         .Range(0, image.Height)
+                         .Select(y => (x, y))))
+            image[x, y] = function(image[x, y]);
+
+        return image;
+    }
+}
---
+++ ComputerVision/ImageOperations.cs
@@ -0,0 +1,60 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace ComputerVision;
+
+public class ImageOperations
+{
+    public static async ValueTask<Stream> ToGrayscaleAsync(Stream imageStream)
+    {
+        using var sourceImage = await Image.LoadAsync<Rgba32>(Configuration.Default, imageStream);
+
+        using var grayscaleImage = sourceImage
+            .Clone()
+            .ForEachPixel(pixel =>
+            {
+                var average = (byte)((pixel.R + pixel.G + pixel.B) / 3);
+                var updatedPixel = new Rgba32(average, average, average, pixel.A);
+
+                return updatedPixel;
+            });
+
+        var resultStream = new MemoryStream();
+        await grayscaleImage.SaveAsync(resultStream, new JpegEncoder());
+        resultStream.Position = 0;
+
+        return resultStream;
+    }
+
+    public static async ValueTask<Stream> ToNoisedAsync(Stream imageStream, double intensity = 0.5, byte mean = 25,
+        byte sigma = 5)
+    {
+        using var sourceImage = await Image.LoadAsync<Rgba32>(Configuration.Default, imageStream);
+
+        var random = new Random(DateTime.UtcNow.Millisecond);
+        using var noisedImage = sourceImage
+            .Clone()
+            .ForEachPixel(pixel =>
+            {
+                if (random.NextDouble() < intensity)
+                    return pixel;
+
+                var gaussian = random.NextGaussian(mean, sigma);
+                var r = (byte)(pixel.R + gaussian);
+                var g = (byte)(pixel.G + gaussian);
+                var b = (byte)(pixel.B + gaussian);
+
+                var updatedPixel = new Rgba32(r, g, b, pixel.A);
+
+                return updatedPixel;
+
+            });
+
+        var resultStream = new MemoryStream();
+        await noisedImage.SaveAsync(resultStream, new JpegEncoder());
+        resultStream.Position = 0;
+
+        return resultStream;
+    }
+}
