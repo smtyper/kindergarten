3bb80a1f4dacd023de80d84612a1e305f47aa802
Artem <smtyper.contact@gmail.com>
2022-12-03 06:17:46 +0300

    Implement ExpertSystemWrapper

---
+++ ExpertSystemWrapper/FileStorage.cs
@@ -0,0 +1,65 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ExpertSystemWrapper;
+
+public static class FileStorage
+{
+    private const string RulesFilePath = "kb.txt";
+    private const string FactsFilePath = "facts.txt";
+    private const string LogsFilePath = "explanation.txt";
+
+    private const string ValueExceptBracket = @"[^\(\)]";
+
+    private static readonly Regex RuleLineRegex = new(@$"\s*(\({ValueExceptBracket}+\)\s*)+=>\s*(\({ValueExceptBracket}+\)\s*)+",
+        RegexOptions.Compiled);
+    private static readonly Regex FactLineRegex = new($@"\s*\({ValueExceptBracket}+\)\s*", RegexOptions.Compiled);
+    private static readonly Regex ValueBetweenBracketsRegex = new(@$"(?<=\(){ValueExceptBracket}+(?=\))",
+        RegexOptions.Compiled);
+
+    public static async ValueTask<IReadOnlyCollection<(IReadOnlyCollection<string> Conditions,
+            IReadOnlyCollection<string> Conclusions)>> ReadRulesFileAsync()
+    {
+        var lines = await File.ReadAllLinesAsync(RulesFilePath);
+
+        foreach (var line in lines)
+            if (!RuleLineRegex.IsMatch(line))
+                throw new Exception($"Invalid rule line format: {line}.");
+
+        var conditionsConclusionsPairs = lines
+            .Select(line =>
+            {
+                var splitted = line.Split("=>");
+
+                var conditions = GetValuesFromBrackets(splitted[0]);
+                var conclusions = GetValuesFromBrackets(splitted[1]);
+
+                return (conditions, conclusions);
+            })
+            .ToArray();
+
+        return conditionsConclusionsPairs;
+    }
+
+    public static async ValueTask<IReadOnlyCollection<string>> ReadFactsFileAsync()
+    {
+        var lines = await File.ReadAllLinesAsync(FactsFilePath);
+
+        foreach (var line in lines)
+            if (!FactLineRegex.IsMatch(line))
+                throw new Exception($"Invalid file line format: {line}.");
+
+        var facts = lines
+            .Select(line => GetValuesFromBrackets(line).Single())
+            .ToArray();
+
+        return facts;
+    }
+
+    public static async ValueTask WriteLogsList(IReadOnlyCollection<string> logs) => await File.WriteAllLinesAsync(
+        LogsFilePath, logs);
+
+    private static IReadOnlyCollection<string> GetValuesFromBrackets(string text) => ValueBetweenBracketsRegex
+        .Matches(text)
+        .Select(match => match.Value.Trim())
+        .ToArray();
+}
---
+++ ExpertSystemWrapper/Program.cs
@@ -0,0 +1,35 @@
+﻿using ExpertSystemWrapper;
+
+var rulesPairs = await FileStorage.ReadRulesFileAsync();
+var factsList = (await FileStorage.ReadFactsFileAsync()).ToList();
+
+var rulesMapper = new DynamicRulesMapper(rulesPairs);
+var logsList = new List<string>();
+
+var iterationIndex = 0;
+
+while (true)
+{
+    var conflictSet = rulesMapper.GetConflictSet(factsList);
+    var (selectedRuleIndex, selectedRule) = rulesMapper.SelectRule(factsList);
+
+    if (selectedRule is null)
+        break;
+
+    factsList.AddRange(selectedRule.Conclusions);
+    LogSelection(conflictSet, selectedRuleIndex!.Value);
+    iterationIndex++;
+}
+
+await FileStorage.WriteLogsList(logsList);
+
+void LogSelection(IReadOnlyCollection<(int Index, DynamicRulesMapper.Rule Rule)> conflictSet, int selectedRuleIndex)
+{
+    var logMessage =
+        $"{iterationIndex}. ({string.Join(", ", conflictSet.Select(pair => pair.Index))}); ({selectedRuleIndex})";
+
+    logsList.Add(logMessage);
+    Console.WriteLine($"[{DateTime.UtcNow}]: {logMessage}");
+}
+
+
