1ef2d3aa7e0b252d4c33e60af5bd6afbf6f53c1b
Artem <smtyper.contact@gmail.com>
2023-08-08 10:55:30 +0300

    Stash

---
+++ Itis/Program.cs
@@ -0,0 +1,112 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+using AngleSharp;
+using AngleSharp.Dom;
+using AngleSharp.Html.Dom;
+
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+using var httpClient = new HttpClient { BaseAddress = new Uri("https://abiturient.kpfu.ru/") };
+await using var htmlStream = await httpClient.GetStreamAsync("/entrant/abit_entrant_originals_list?p_inst=0&p_faculty=47&p_speciality=767&p_typeofstudy=1&p_category=1");
+using var streamReader = new StreamReader(htmlStream, Encoding.GetEncoding(1251));
+
+var htmlString = await streamReader.ReadToEndAsync();
+
+var configuration = Configuration.Default;
+using var context = new BrowsingContext(configuration);
+using var document = await context.OpenAsync(response => response.Content(htmlString));
+
+var participants = document
+    .QuerySelectorAll<IHtmlTableRowElement>("#t_common > tbody > tr")
+    .Skip(2)
+    .Select(rowElement =>
+    {
+        var numberInListString = rowElement.Cells[0].TextContent.Trim();
+        var idString = rowElement.Cells[1].TextContent.Trim();
+        var scoreString = rowElement.Cells[2].TextContent.Trim();
+        var isPreemptiveRightString = rowElement.Cells[3].TextContent.Trim();
+        var priorityString = rowElement.Cells[4].TextContent.Trim();
+        var isOriginalDocumentString = rowElement.Cells[5].TextContent.Trim();
+        var statusString = rowElement.Cells[6].TextContent.Trim();
+        var commentString = rowElement.Cells[7].TextContent.Trim();
+
+        var participant = new Participant
+        {
+            NumberInList = int.Parse(numberInListString),
+            Id = idString,
+            Score = string.IsNullOrEmpty(scoreString) ?
+                null :
+                decimal.Parse(scoreString),
+            IsPreemptiveRight = isPreemptiveRightString switch
+            {
+                "да" => true,
+                "нет" => false,
+                _ => throw new ArgumentOutOfRangeException(nameof(isPreemptiveRightString))
+            },
+            Priority = int.Parse(priorityString),
+            IsOriginalDocument = isOriginalDocumentString switch
+            {
+                "да" => true,
+                "нет" => false,
+                _ => throw new ArgumentOutOfRangeException(nameof(isPreemptiveRightString))
+            },
+            Status = statusString,
+            Comment = commentString
+        };
+
+        return participant;
+    })
+    .ToArray();
+
+var userParticipant = participants.Single(participant => participant.Id is "163-262-496-63");
+
+var isOriginalPriorityRaiting = participants
+    .Where(participant => participant is { IsOriginalDocument: true, Priority: 1 })
+    .Append(userParticipant with { IsOriginalDocument = true, Priority = 1 })
+    .Distinct()
+    .OrderByDescending(participant => participant.Score)
+    .Select((participant, index) => (place: index + 1, participant))
+    .ToArray();
+var opUserPlace = isOriginalPriorityRaiting.Single(p => p.participant.Id == userParticipant.Id);
+
+var isOriginalRaiting = participants
+    .Where(participant => participant is { IsOriginalDocument: true })
+    .Append(userParticipant with { IsOriginalDocument = true })
+    .Distinct()
+    .OrderByDescending(participant => participant.Score)
+    .Select((participant, index) => (place: index + 1, participant))
+    .ToArray();
+var oUserPlace = isOriginalRaiting.Single(p => p.participant.Id == userParticipant.Id);
+
+var competitors = participants
+    .Where(participant => participant.Id != userParticipant.Id &&
+                                                                   participant.Score >= userParticipant.Score &&
+                                                                   !participant.IsOriginalDocument)
+    .ToArray();
+
+Console.WriteLine($"Your place in original + priority raiting is: {opUserPlace.place}.");
+Console.WriteLine($"Your place in original is: {oUserPlace.place}.");
+Console.WriteLine($"Сompetitors count: {competitors.Length}");
+
+public record Participant
+{
+    public int NumberInList { get; init; }
+
+    public required string Id { get; init; }
+
+    public decimal? Score { get; init; }
+
+    public bool IsPreemptiveRight { get; init; }
+
+    public int Priority { get; init; }
+
+    public bool IsOriginalDocument { get; init; }
+
+    public required string Status { get; init; }
+
+    public string? Comment { get; init; }
+}
+
+
+
---
+++ Itmo/Program.cs
@@ -0,0 +1,71 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AngleSharp;
+
+Console.WriteLine("Hello, World!");
+
+using var httpClient = new HttpClient { BaseAddress = new Uri("https://abit.itmo.ru/") }; //RatingPage_table__FbzTn
+await using var htmlStream = await httpClient.GetStreamAsync("rating/master/budget/7488");
+
+var configuration = Configuration.Default;
+using var context = new BrowsingContext(configuration);
+using var document = await context.OpenAsync(response => response.Content(htmlStream));
+
+var participants = document.QuerySelectorAll(".RatingPage_table__item__qMY0F")
+    .Select(element =>
+    {
+        var priorityTrialTypeBlock = element.QuerySelectorAll(".RatingPage_table__infoLeft__Y_9cA>p>span");
+
+        var id = element.QuerySelector(".RatingPage_table__position__uYWvi>span")!.TextContent;
+        var priority = int.Parse(priorityTrialTypeBlock.First().TextContent);
+        var allPoints = double.Parse(element
+            .QuerySelectorAll(".RatingPage_table__infoLeft__Y_9cA>p")
+            .Single(subElement => subElement.TextContent.Contains("Балл ВИ+ИД: ")).TextContent
+            .Split("Балл ВИ+ИД: ")
+            .Last());
+        var trialType = priorityTrialTypeBlock.Skip(1).First().TextContent;
+        var isOriginalDocument = element
+                .QuerySelectorAll(".RatingPage_table__info__quwhV>div")
+                .Single(subElement => subElement.QuerySelector("p")!.TextContent.Contains("Оригиналы документов: "))
+                .QuerySelector("span")?.TextContent switch
+                {
+                    "да" => true,
+                    "нет" => false,
+                    _ => throw new NotImplementedException()
+                };
+
+        var participant = new Participant
+        {
+            Id = id,
+            Priority = priority,
+            AllPoints = allPoints,
+            TrialType = trialType,
+            IsOriginalDocument = isOriginalDocument
+        };
+
+        return participant;
+    })
+    .ToArray();
+
+var temp = participants
+    .Where(participant => participant is { AllPoints: >= 100, IsOriginalDocument: true })
+    .ToArray();
+
+var count = temp.Length;
+
+Console.WriteLine();
+
+public record Participant
+{
+    public required string Id { get; init; }
+
+    public int Priority { get; init; }
+
+    public double AllPoints { get; init; }
+
+    public required string TrialType { get; init; }
+
+    public bool IsOriginalDocument { get; init; }
+}
+
+
